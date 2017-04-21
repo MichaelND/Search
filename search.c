47c0c114e77c546607df6c3b371c98fb7a27c526
pbaumann <pbaumann@nd.edu>
2017-04-21 12:03:05 -0400

    search logic still not fixed

--- search.c
+++ search.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #include <dirent.h>
 
@@ -17,6 +18,7 @@
  */
 int	    search(const char *root, const Settings *settings) {	
     struct dirent * dentry;
+    struct stat inode;
     DIR * parent_dir = opendir(root);
     char path[BUFSIZ];
     if (parent_dir == NULL) { //check if directory is openable
@@ -24,12 +26,28 @@ int	    search(const char *root, const Settings *settings) {
     }
     while ((dentry = readdir(parent_dir)) != NULL) { //read
         sprintf(path, "%s/%s", root, dentry->d_name);
-        if (filter(path, settings) == false && (strcmp(dentry->d_name,".") != 0) && (strcmp(dentry->d_name,"..") != 0))  {
+        if (lstat(path, &inode) != 0) {
+            if (S_ISREG(inode.st_mode)) {
+                    if ((strcmp(dentry->d_name, ".") != 0) && (strcmp(dentry->d_name, "..") != 0 ) ) {
+                       search(path,settings);
+                    }
+            }
+            else if (filter(path,settings) == false) {
+               execute(path,settings);
+            }
+        }
+
+        // if is directory
+        //      if d_name != . .. 
+        // else filter, execute
+        /*
+        if ((strcmp(dentry->d_name,".") != 0) && (strcmp(dentry->d_name,"..") != 0) && (filter(path,settings) == false))  {
             execute(path, settings);
             if (is_directory_empty(path)) {
                     search(path,settings);
             }
         }
+        */
     }
     closedir(parent_dir); //close
     return EXIT_SUCCESS;
